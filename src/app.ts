d59b169b8dec289b6a0c574979e33fd27a3fa3ff
myroslav <myrkozar@gmail.com>
2022-07-26 16:05:10 +0300

    add task_2

--- src/app.ts
+++ src/app.ts
@@ -216,3 +216,171 @@
 // boing.sitInPlane(pilot)
 
 // boing.startEngin()
+/*-----------------Type intersection----------*/
+// type Admin = {
+//     name: string;
+//     privileges: string[];
+//   };
+  
+//   type Employee = {
+//     name: string;
+//     startDate: Date;
+//   };
+
+//   type ElevatedEmployee = Admin & Employee;
+
+// const e1: ElevatedEmployee = {
+//   name: 'Anton',
+//   privileges: ['drop-all'],
+//   startDate: new Date()
+// };
+
+/*-----------------Type Guards----------*/
+// type Admin = {
+//     name: string;
+//     privileges: string[];
+//   };
+  
+//   type Employee = {
+//     name: string;
+//     startDate: Date;
+//   };
+  
+//   type UnknownObject = Employee | Admin;
+  
+//   function showFields (e1: UnknownObject) {
+//     console.log(e1.name);
+//     if ('privileges' in e1) {
+//       console.log(e1.privileges);
+//     }
+//     if ('startDate' in e1) {
+//       console.log(e1.startDate);
+//     }
+//   }
+
+//   class Car {
+//     drive() {
+//       console.log('Driving...');
+//     }
+//   }
+  
+//   class Truck {
+//     drive() {
+//       console.log('Driving a truck...');
+//     }
+  
+//     loadCargo(amount: number) {
+//       console.log('Loading cargo ...' + amount);
+//     }
+//   }
+  
+//   type Vehicle = Car | Truck;
+  
+//   const v1 = new Car();
+//   const v2 = new Truck();
+  
+//   function useVehicle(vehicle: Vehicle) {
+//     vehicle.drive();
+//     if (vehicle instanceof Truck) {
+//       vehicle.loadCargo(1000);
+//     }
+//   }
+
+/*-----------------Type Casting----------*/
+// const input = <HTMLInputElement>document.getElementById('inputEmail');
+
+// input.value = 'test@test.ts';
+
+// const input = document.getElementById('inputEmail') as HTMLInputElement;
+
+// input.value = 'test@test.ts';
+
+// const input = document.getElementById('inputEmail');
+
+// if (input) {
+//   (input as HTMLInputElement).value = 'test@test.ts';
+// }
+/*-----------------Index Properties----------*/
+// interface Person {
+//     name: string;
+//     [x: string]: string;
+//   }
+  
+//   const user: Person = {
+//     name: 'Alex',
+//     gender: 'MAN',
+//     country: 'Ukraine',
+//   }
+/*-----------------Optional Chaining----------*/
+// interface Person {
+//     name: string;
+//     additionInfo?: {
+//       someInfo: string;
+//     }
+//   }
+  
+//   const user: Person = {
+//     name: 'Alex'
+//   }
+  
+//   console.log(user?.additionInfo?.someInfo);
+/*-----------------Nullish Coalescing----------*/
+// const userInput = '';
+
+// const store = userInput || 'DEFAULT';
+
+// console.log(store);
+
+// const userInput = '';
+
+// const store = userInput ?? 'DEFAULT';
+
+
+// console.log(store);
+// И теперь DEFAULT будет только тогда, когда userInput будет равно null или undefined.
+
+/*-----------------function overloads----------*/
+// type Combinable = string | number;
+
+// function add(a: number, b: number): number;
+// function add(a: string, b: string): string;
+// function add(a: string, b: number): string;
+// function add(a: number, b: string): string;
+// function add(a: Combinable, b: Combinable) {
+//   if (typeof a === 'string' || typeof b === 'string') {
+//     return a.toString() + b.toString();
+//   }
+//   return a + b;
+// }
+
+// type AdminType = {
+//     type: 'admin';
+//     name: string;
+//   }
+  
+//   type UserType = {
+//     type: 'user';
+//     name: string;
+//   }
+  
+//   function checkUser (name: string, type: 'admin'): AdminType;
+//   function checkUser (name: string, type: 'user'): UserType;
+//   function checkUser (name: string, type: 'admin' | 'user') {
+//     if (type === 'admin') {
+//       return {
+//         name,
+//         type: 'admin'
+//       }
+//     } else {
+//       return {
+//         name,
+//         type: 'user'
+//       }
+//     }
+//   }
+  
+//   const user = checkUser('Nikita', 'user');
+//   const admin = checkUser('Tonya', 'admin');
+
+/*----------------------------------------------------------------------------------*/ 
+/*-----------------Generics----------*/
